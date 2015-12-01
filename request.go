c0de896560e1b4ab3daca93d78b6992d7cc586be
Frederick Akalin <akalin@gmail.com>
2015-11-30 20:43:05 -0800

    Add TODOs

--- request.go
+++ request.go
@@ -75,7 +75,9 @@ func (r *callRequest) Reply(enc encoder, log LogInterface) error {
 	var err error
 	select {
 	case <-r.ctx.Done():
+		// TODO: Use newCanceledError.
 		err = fmt.Errorf("call canceled for seqno %d", r.seqno)
+		// TODO: Use log.Info instead.
 		log.Warning(err.Error())
 	default:
 		v := []interface{}{
