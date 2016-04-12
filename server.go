9f2a32bf8fd7a42f6c63cd7b5501ca043df826a8
Frederick Akalin <akalin@gmail.com>
2016-04-12 11:19:48 -0700

    Add comments

--- server.go
+++ server.go
@@ -14,8 +14,14 @@ func (s *Server) Register(p Protocol) error {
 	return s.xp.RegisterProtocol(p)
 }
 
-// Run starts processing incoming RPC messages synchronously. Always
-// returns a non-nil error, e.g. io.EOF if the connection was closed.
+// Run starts processing incoming RPC messages synchronously.
+//
+// Note that an error is returned immediately if incoming messages are
+// already being processed, e.g. if the underlying transport has been
+// used to make an RPC call (which has to wait for a
+// reply). Otherwise, the returned error (which is always non-nil,
+// e.g. io.EOF if the connection was closed) is due to an error
+// processing a frame or the connection being closed.
 func (s *Server) Run() error {
 	return s.xp.ReceiveFrames()
 }
