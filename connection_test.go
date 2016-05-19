eb7d83bc95dce0c4bd1a7a4562e37056bc285c03
Fred Akalin <akalin@keyba.se>
2016-05-19 15:53:04 -0700

    Put some test code in connection_test_util.go (#91)
    This makes it easier to write some
    external RPC tests.

--- connection_test.go
+++ connection_test.go
@@ -3,8 +3,6 @@ package rpc
 import (
 	"errors"
 	"fmt"
-	"io"
-	"net"
 	"testing"
 	"time"
 
@@ -100,74 +98,6 @@ func (ut *unitTester) Err() error {
 	return nil
 }
 
-type testStatus struct {
-	Code int
-}
-type testUnwrapper struct{}
-
-func testWrapError(err error) interface{} {
-	return &testStatus{}
-}
-
-func testLogTags(ctx context.Context) (map[interface{}]string, bool) {
-	return nil, false
-}
-
-type throttleError struct {
-	Err error
-}
-
-func (e throttleError) ToStatus() (s testStatus) {
-	s.Code = 15
-	return
-}
-
-func (e throttleError) Error() string {
-	return e.Err.Error()
-}
-
-type testErrorUnwrapper struct{}
-
-var _ ErrorUnwrapper = testErrorUnwrapper{}
-
-func (eu testErrorUnwrapper) MakeArg() interface{} {
-	return &testStatus{}
-}
-
-func (eu testErrorUnwrapper) UnwrapError(arg interface{}) (appError error, dispatchError error) {
-	s, ok := arg.(*testStatus)
-	if !ok {
-		return nil, errors.New("Error converting arg to testStatus object")
-	}
-	if s == nil || s.Code == 0 {
-		return nil, nil
-	}
-
-	switch s.Code {
-	case 15:
-		appError = throttleError{errors.New("throttle")}
-		break
-	default:
-		panic("Unknown testing error")
-	}
-	return appError, nil
-}
-
-type testLogOutput struct {
-	t *testing.T
-}
-
-func (t testLogOutput) log(ch string, fmts string, args []interface{}) {
-	fmts = fmt.Sprintf("[%s] %s\n", ch, fmts)
-	t.t.Logf(fmts, args...)
-}
-
-func (t testLogOutput) Info(fmt string, args ...interface{})    { t.log("I", fmt, args) }
-func (t testLogOutput) Error(fmt string, args ...interface{})   { t.log("E", fmt, args) }
-func (t testLogOutput) Debug(fmt string, args ...interface{})   { t.log("D", fmt, args) }
-func (t testLogOutput) Warning(fmt string, args ...interface{}) { t.log("W", fmt, args) }
-func (t testLogOutput) Profile(fmt string, args ...interface{}) { t.log("P", fmt, args) }
-
 // Test a basic reconnect flow.
 func TestReconnectBasic(t *testing.T) {
 	unitTester := &unitTester{
@@ -247,72 +177,8 @@ func TestDoCommandThrottle(t *testing.T) {
 	}
 }
 
-type testConnectionHandler struct{}
-
-var _ ConnectionHandler = testConnectionHandler{}
-
-func (testConnectionHandler) OnConnect(context.Context, *Connection, GenericClient, *Server) error {
-	return nil
-}
-
-func (testConnectionHandler) OnConnectError(err error, reconnectThrottleDuration time.Duration) {
-}
-
-func (testConnectionHandler) OnDoCommandError(err error, nextTime time.Duration) {
-}
-
-func (testConnectionHandler) OnDisconnected(ctx context.Context, status DisconnectStatus) {
-}
-
-func (testConnectionHandler) ShouldRetry(name string, err error) bool {
-	return false
-}
-
-func (testConnectionHandler) ShouldRetryOnConnect(err error) bool {
-	return false
-}
-
-func (testConnectionHandler) HandlerName() string {
-	return "testConnectionHandler"
-}
-
-type sharedTransport struct {
-	t Transporter
-}
-
-var _ ConnectionTransport = sharedTransport{}
-
-// Dial is an implementation of the ConnectionTransport interface.
-func (st sharedTransport) Dial(ctx context.Context) (Transporter, error) {
-	if !st.t.IsConnected() {
-		return nil, io.EOF
-	}
-	return st.t, nil
-}
-
-// IsConnected is an implementation of the ConnectionTransport interface.
-func (st sharedTransport) IsConnected() bool {
-	return st.t.IsConnected()
-}
-
-// Finalize is an implementation of the ConnectionTransport interface.
-func (st sharedTransport) Finalize() {}
-
-// Close is an implementation of the ConnectionTransport interface.
-func (st sharedTransport) Close() {}
-
-func makeConnectionForTest(t *testing.T) (net.Conn, *Connection) {
-	clientConn, serverConn := net.Pipe()
-	transporter := NewTransport(clientConn, nil, testWrapError)
-	st := sharedTransport{transporter}
-	output := testLogOutput{t}
-	conn := NewConnectionWithTransport(testConnectionHandler{}, st,
-		testErrorUnwrapper{}, true, testWrapError, output, testLogTags)
-	return serverConn, conn
-}
-
 func TestConnectionClientCallError(t *testing.T) {
-	serverConn, conn := makeConnectionForTest(t)
+	serverConn, conn := MakeConnectionForTest(t)
 	defer conn.Shutdown()
 
 	c := connectionClient{conn}
@@ -326,7 +192,7 @@ func TestConnectionClientCallError(t *testing.T) {
 }
 
 func TestConnectionClientNotifyError(t *testing.T) {
-	serverConn, conn := makeConnectionForTest(t)
+	serverConn, conn := MakeConnectionForTest(t)
 	defer conn.Shutdown()
 
 	c := connectionClient{conn}
@@ -340,7 +206,7 @@ func TestConnectionClientNotifyError(t *testing.T) {
 }
 
 func TestConnectionClientCallCancel(t *testing.T) {
-	serverConn, conn := makeConnectionForTest(t)
+	serverConn, conn := MakeConnectionForTest(t)
 	defer conn.Shutdown()
 
 	c := connectionClient{conn}
@@ -362,7 +228,7 @@ func TestConnectionClientCallCancel(t *testing.T) {
 }
 
 func TestConnectionClientNotifyCancel(t *testing.T) {
-	serverConn, conn := makeConnectionForTest(t)
+	serverConn, conn := MakeConnectionForTest(t)
 	defer conn.Shutdown()
 
 	c := connectionClient{conn}
