cc731653f603863f770e10ff5da03d8a6ea8dcd3
Jack O'Connor <oconnor663@gmail.com>
2017-07-24 12:38:59 -0400

    make the -lresolv CGO flag Darwin-only

---
+++ rpc/resinit/resinit_darwin.go
@@ -0,0 +1,18 @@
+// Copyright 2015 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+// +build darwin
+
+package resinit
+
+// NOTE: The Darwin build requires -lresolv, but that option breaks FreeBSD.
+// This implementation is split out from resinit_nix.go for that reason. See
+// https://github.com/keybase/keybase-issues/issues/3022.
+
+// #cgo LDFLAGS: -lresolv
+// #include<resolv.h>
+import "C"
+
+func resInit() {
+	C.res_init()
+}
